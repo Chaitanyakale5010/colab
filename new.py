b3460a65115c222a3422ed4ad512cfd8f1a1a819
Chaitanyakale5010 <152631436+Chaitanyakale5010@users.noreply.github.com>
2024-05-08 19:28:33 +0530

    python opps program

---
+++ new.py
@@ -0,0 +1,14 @@
+class person :
+  name ="Chaitanya"
+  age=19
+
+obj = person()
+print("Hi my name is",obj.name,"and age is ",obj.age)
+obj.hobby = "Writing"
+obj.work="devloper"
+print(obj.hobby)
+print(obj.work)
+
+print(per.age)#if i try to crete a new object then additional propertiess are not valid
+per = person()
+print(per.name)
